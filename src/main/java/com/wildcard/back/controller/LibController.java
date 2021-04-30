1863306b0ec11f97fc3e3d6ad769d7c1de6a94e6
Pavlo Zaviazun <help4engineer@gmail.com>
2021-04-30 11:45:46 +0300

    master

--- src/main/java/com/wildcard/back/controller/LibController.java
+++ src/main/java/com/wildcard/back/controller/LibController.java
@@ -2,6 +2,7 @@ package com.wildcard.back.controller;
 
 import com.wildcard.back.dao.LibDAO;
 import com.wildcard.back.models.Lib;
+import com.wildcard.back.models.Word;
 import com.wildcard.back.util.Validation;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.*;
@@ -23,14 +24,35 @@ public class LibController {
     }
 
     @GetMapping("/lib/{id}/get")
-    public void getLib(@PathVariable int id) {
-
+    public Lib getLib(@PathVariable int id) {
+        if(libDAO.findById(id).isPresent()) {
+            return libDAO.findById(id).get();
+        }
+        return new Lib();
     }
+
+    @DeleteMapping("/lib/{id}/delete")
+    public void deleteLib(@PathVariable int id) {
+        libDAO.deleteById(id);
+    }
+
+    @PatchMapping("/lib/{id}/update")
+    public void updateLib(@PathVariable int id,
+                          @RequestParam String name) {
+        Lib libObj = libDAO.getOne(id);
+        if(!libObj.getName().equals(name)) {
+            String nameRequest = Validation.sentenceValidation(name);
+            if(nameRequest != null) {
+                libObj.setName(nameRequest);
+                libDAO.save(libObj);
+            }
+        }
+    }
+
+
 
     @GetMapping("/libs/get")
     public List <Lib> getLibs() {
         return libDAO.findAll();
     }
-
-
 }
