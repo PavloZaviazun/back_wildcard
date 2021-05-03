6c7dfc44251f626b0b93b5b0d7d25f9c1ff214bc
Pavlo Zaviazun <help4engineer@gmail.com>
2021-05-03 16:53:49 +0300

    master

---
+++ src/main/java/com/wildcard/back/configs/SecurityConfig.java
@@ -0,0 +1,25 @@
+package com.wildcard.back.configs;
+
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+//import org.springframework.security.core.userdetails.UserDetailsService;
+//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+//import org.springframework.security.crypto.password.PasswordEncoder;
+//
+//public class SecurityConfig extends WebSecurityConfigurerAdapter {
+//
+//    private UserDetailsService userDetailsService;
+//    @Bean
+//    public PasswordEncoder passwordEncoder() {
+//        return new BCryptPasswordEncoder();
+//    }
+//
+//    @Bean
+//    public DaoAuthenticationProvider daoAuthenticationProvider() {
+//        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+//        provider.setPasswordEncoder(new BCryptPasswordEncoder());
+//        provider.setUserDetailsService(userDetailsService);
+//        return provider;
+//    }
+//}
