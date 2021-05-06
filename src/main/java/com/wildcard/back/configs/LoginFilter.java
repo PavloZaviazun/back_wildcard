888dca3ef135f53a41deb7f86e3a77a32fd23648
Pavlo Zaviazun <help4engineer@gmail.com>
2021-05-06 12:43:28 +0300

    master

--- src/main/java/com/wildcard/back/configs/LoginFilter.java
+++ src/main/java/com/wildcard/back/configs/LoginFilter.java
@@ -1,49 +1,53 @@
 package com.wildcard.back.configs;
 
-//import com.fasterxml.jackson.databind.ObjectMapper;
-//import com.wildcard.back.dao.UserDAO;
-//import com.wildcard.back.models.User;
-//import io.jsonwebtoken.Jwts;
-//import io.jsonwebtoken.SignatureAlgorithm;
-//import org.springframework.security.authentication.AuthenticationManager;
-//import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-//import org.springframework.security.core.Authentication;
-//import org.springframework.security.core.AuthenticationException;
-//import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-//
-//import javax.servlet.FilterChain;
-//import javax.servlet.ServletException;
-//import javax.servlet.http.HttpServletRequest;
-//import javax.servlet.http.HttpServletResponse;
-//import java.io.IOException;
-//
-//public class LoginFilter extends UsernamePasswordAuthenticationFilter {
-//    private UserDAO userDAO;
-//    public LoginFilter(String url, AuthenticationManager authenticationManager, UserDAO userDAO) {
-//
-//    }
-//
-//    @Override
-//    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
-//        User user = null;
-//        try {
-//            user = new ObjectMapper().readValue(request.getInputStream(), User.class);
-//        } catch (IOException e) {
-//            System.out.println(e.getMessage());
-//        }
-//        Authentication authenticate = getAuthenticationManager().authenticate(
-//                //TODO could be getUserName();
-//                new UsernamePasswordAuthenticationToken(user.getEmail(), user.getPassword()));
-//        return authenticate;
-//    }
-//
-//    @Override
-//    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authResult) throws IOException, ServletException {
-//        String token = Jwts.builder()
-//                .setSubject(authResult.getName())
-//                .signWith(SignatureAlgorithm.HS512, "WildCard".getBytes())
-//                .compact();
-//        System.out.println(authResult);
-////        userDAO.findUserByEmail(authResult.get)
-//    }
-//}
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.wildcard.back.dao.UserDAO;
+import com.wildcard.back.models.AuthToken;
+import com.wildcard.back.models.User;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class LoginFilter extends UsernamePasswordAuthenticationFilter {
+    private UserDAO userDAO;
+    public LoginFilter(String url, AuthenticationManager authenticationManager, UserDAO userDAO) {
+
+    }
+
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
+        User user = null;
+        try {
+            user = new ObjectMapper().readValue(request.getInputStream(), User.class);
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+        Authentication authenticate = getAuthenticationManager().authenticate(
+                //TODO could be getUserName();
+                new UsernamePasswordAuthenticationToken(user.getEmail(), user.getPassword()));
+        return authenticate;
+    }
+
+    @Override
+    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authResult) throws IOException, ServletException {
+        String token = Jwts.builder()
+                .setSubject(authResult.getName())
+                .signWith(SignatureAlgorithm.HS512, "WildCard".getBytes())
+                .compact();
+        System.out.println(authResult);
+        User user = userDAO.findUserByEmail(authResult.getName());
+        AuthToken authToken = new AuthToken();
+        authToken.setToken(token);
+        authToken.setUser(user);
+    }
+}
--- src/main/java/com/wildcard/back/controller/MainController.java
+++ src/main/java/com/wildcard/back/controller/MainController.java
@@ -6,9 +6,6 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Arrays;
-import java.util.List;
-
 @AllArgsConstructor
 @RestController
 @CrossOrigin(origins = "http://localhost:3000")
