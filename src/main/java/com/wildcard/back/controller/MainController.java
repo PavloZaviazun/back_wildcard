ad4d779c2bd78cda1f87635da508fbcbb57a8682
Pavlo Zaviazun <help4engineer@gmail.com>
2021-05-07 10:15:40 +0300

    master

--- src/main/java/com/wildcard/back/controller/MainController.java
+++ src/main/java/com/wildcard/back/controller/MainController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @AllArgsConstructor
 @RestController
-//@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "http://localhost:3000")
 public class MainController {
 
     @GetMapping("/partsOfSpeech")
--- src/main/java/com/wildcard/back/controller/WordController.java
+++ src/main/java/com/wildcard/back/controller/WordController.java
@@ -16,7 +16,7 @@ import java.util.List;
 
 @AllArgsConstructor
 @RestController
-//@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "http://localhost:3000")
 public class WordController {
     private LibDAO libDAO;
     private WordDAO wordDAO;
